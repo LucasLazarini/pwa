f1b95b3e5a649f461fc603c43018d153b5c4a3a4
Lucas Lazarini <37162691+LucasLazarini@users.noreply.github.com>
2019-01-07 19:09:52 -0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+if (navigator.serviceWorker){
+  navigator.serviceWorker.register('/sw.js')
+  .then(function(){
+    console.log("sw registrado");
+  })
+  .catch(function(err){
+    console.log("sw não funciona: "+err);
+  });
+} else {
+  console.log("SW não suportado. Atualize o navegador ou vá embora.")
+}
